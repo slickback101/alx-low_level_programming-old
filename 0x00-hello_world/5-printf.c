bf07a0b7b546d9260a6e2b8db218b54768920429
slickback101 <kester.uzum@gmail.com>
2023-06-15 15:59:29 -0700

    C-Programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#incude<stdio.h>
+/**
+ *
+ * main - Entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+init main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+
+	return (0);
+}
