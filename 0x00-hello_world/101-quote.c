483f5a46030c8e7429f78965e0a2ffece84e45ef
slickback101 <kester.uzum@gmail.com>
2023-06-15 18:52:04 -0700

    C Programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,7 @@
-#include<stdio.h>
-#include<unistd.h>
-/**
- * main - A C program that prints a line to the standard erroe
- * Return 1 (Success)
- */
-int main(void)
-{
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+#include <unistd.h>
+
+int main() {
+    const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, 59);
+    return 1;
 }
