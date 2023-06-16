b38a76ae884952744b01b90a0909b17c7a331699
slickback101 <kester.uzum@gmail.com>
2023-06-15 18:59:58 -0700

    C Programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 
-int main() {
+int main(void)
+{
     const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(2, message, 59);
     return 1;
