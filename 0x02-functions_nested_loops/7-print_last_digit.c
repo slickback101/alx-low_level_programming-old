e8ec0aa21f42837364c8cca3dab72a44d9c89c69
slickback101 <kester.uzum@gmail.com>
2023-06-20 21:31:16 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Prints the last digit of a number
+ * @c: The number to be treated
+ * Return: Value of the las digit of the number
+ */
+
+int print_last_digit(int c)
+{
+	int last;
+	last = c % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
