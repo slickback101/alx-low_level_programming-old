23fca0417b29acb23fba423ac685fa63886d825d
slickback101 <kester.uzum@gmail.com>
2023-06-26 17:45:03 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,14 @@
 void print_rev(char *s)
 {
 	int c = 0;
-	
+
 	while (s[c] != '\0')
 	{
 		c++;
 	}
 	for (c -= 1; c >= 0; c--)
-			{
-				_putchar(s[c]);
-			}
+	{
+		_putchar(s[c]);
+	}
 	_putchar('\n');
 }
